91add3304e4b25ebb4ff3a7cc6becee45448ca24
Philip Schill <philip.schill@gmx.de>
2016-06-27 23:44:41 +0200

    Added get_setting / set_setting to DatabaseConnector.

--- core/database_connector.py
+++ core/database_connector.py
@@ -1,4 +1,6 @@
 import logging
+import datetime
+import json
 import sqlite3
 
 
@@ -10,6 +12,7 @@ class DatabaseConnector(object):
         overwritten.
         :param database_location: Path to the database.
         """
+        self._date_format = "%Y-%m-%dT%H:%M:%S:%f"
         self._connection = sqlite3.connect(database_location)
         c = self._connection.cursor()
         c.execute("""CREATE TABLE IF NOT EXISTS `Users` (
@@ -45,6 +48,14 @@ class DatabaseConnector(object):
         self._connection.commit()
         # select * from sqlite_master where type='table';
 
+    @property
+    def date_format(self):
+        """
+        Returns the date format.
+        :return: The date format.
+        """
+        return self._date_format
+
     def __del__(self):
         """
         Closes the database connection.
@@ -62,17 +73,76 @@ class DatabaseConnector(object):
 
     def get_user_uid(self, name):
         """
-        Returns the uid of the given user. If the name does not exist, it will be inserted.
+        Returns the uid of the given user, or None if the name does not exist.
         :param name: The name.
         :return: The uid.
         """
+        assert isinstance(name, str)
+        uid = None
         c = self._connection.cursor()
         c.execute("SELECT `uid` FROM Users WHERE `name`=?;", (name,))
         result = c.fetchone()
         if result is not None:
             uid = result[0]
-        else:
+        return uid
+
+    def get_or_create_user_uid(self, name):
+        """
+        Returns the uid of the given user. If the name does not exist, it will be inserted.
+        :param name: The name.
+        :return: The uid.
+        """
+        assert isinstance(name, str)
+        uid = self.get_user_uid(name)
+        if uid is None:
+            c = self._connection.cursor()
             c.execute("INSERT INTO Users (`name`) VALUES (?);", (name,))
             self._connection.commit()
             uid = c.lastrowid
         return uid
+
+    def set_setting(self, user_uid, key, value):
+        """
+        Adds a setting entry to the database. Throws a TypeError if value cannot be converted to JSON via json.dumps.
+        :param user_uid: The user uid.
+        :param key: The setting key.
+        :param value: The setting value.
+        """
+        assert isinstance(user_uid, int)
+        assert isinstance(key, str)
+        s = json.dumps(value)
+        timestamp = self._get_current_timestamp()
+        c = self._connection.cursor()
+        c.execute("INSERT INTO Settings (`user_uid`, `timestamp_create`, `key`, `value`) VALUES (?, ?, ?, ?);",
+                  (user_uid, timestamp, key, s))
+
+    def get_setting(self, user_uid, key, default=None):
+        """
+        Returns the setting value for the given user.
+        If there is no stored setting entry and default is set, default is returned. If there is no stored setting entry
+        and default is not set, a KeyError is thrown.
+        :param user_uid: The user uid.
+        :param key: The setting key.
+        :param default: The default value.
+        :return: The setting value.
+        """
+        assert isinstance(user_uid, int)
+        assert isinstance(key, str)
+        c = self._connection.cursor()
+        c.execute("SELECT `timestamp_create`, `value` FROM Settings WHERE `user_uid`=? AND `key`=? "
+                  "ORDER BY `timestamp_create` DESC;", (user_uid, key))
+        row = c.fetchone()
+        value = default
+        if row is not None:
+            value = json.loads(row[1])
+        elif default is None:
+            raise KeyError("Not setting found with user_uid=%s and key=%s." % (user_uid, key))
+        return value
+
+    def _get_current_timestamp(self):
+        """
+        Returns a well-formatted current timestamp.
+        :return: The timestamp.
+        """
+        now = datetime.datetime.now()
+        return now.strftime(self.date_format)
--- test/test_database.py
+++ test/test_database.py
@@ -11,7 +11,6 @@ db = None
 def setUpModule():
     """
     Create the database.
-    :return:
     """
     global db
     db = DatabaseConnector(DB_PATH)
@@ -32,11 +31,38 @@ class TestDatabase(unittest.TestCase):
         """
         Create multiple users and make sure that the uids different and greater than zero.
         """
-        uid0 = db.get_user_uid("Martin")
-        uid1 = db.get_user_uid("Peter")
+        uid0 = db.get_or_create_user_uid("Martin")
+        uid1 = db.get_or_create_user_uid("Peter")
         self.assertGreater(uid0, 0)
         self.assertGreater(uid1, 0)
         self.assertNotEqual(uid0, uid1)
+
+    def test_get_user_uid(self):
+        """
+        Check that the uid is consistent between get_or_create_user_uid() and get_user_uid().
+        """
+        uid0 = db.get_or_create_user_uid("Hans")
+        uid1 = db.get_user_uid("Hans")
+        self.assertGreater(uid0, 0)
+        self.assertEqual(uid0, uid1)
+
+    def test_settings(self):
+        """
+        Check the get and set settings functions.
+        """
+        uid = db.get_or_create_user_uid("Thomas")
+        with self.assertRaises(KeyError):
+            value = db.get_setting(uid, "testkey")
+        value = db.get_setting(uid, "testkey", default=5)
+        self.assertEqual(value, 5)
+        db.set_setting(uid, "testkey", 2)
+        value = db.get_setting(uid, "testkey")
+        self.assertEqual(value, 2)
+        value = db.get_setting(uid, "testkey", default=5)
+        self.assertEqual(value, 2)
+        db.set_setting(uid, "testkey", 6)
+        value = db.get_setting(uid, "testkey")
+        self.assertEqual(value, 6)
 
 
 if __name__ == "__main__":
